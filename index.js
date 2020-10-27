c0ca8421e347f7e0efae72f3542b161fad67d94c
Laurel Kelly <laurellkelly@gmail.com>
2020-10-27 11:38:59 -0700

    Commit all the files

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // write your code here
+const http = require('http');
+
+const server = http.createServer(function (req, res) {
+    console.log('Received' + req.method + ' request for: ' + req.url);
+    
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+
+    res.end('Hello World');
+});
+
+server.listen(8080, 'localhost', null, function() {
+    console.log('Server is listening on localhost:8080');
+});
